26fc711a3dccdd836eb70c01be83f6fab7b45192
hanifuddin15 <hanifuddin.buet@outlook.com>
2022-11-13 12:02:47 +0600

    Add project files.

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace BookListRazor.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+
+        { 
+        
+        }
+
+        
+            public DbSet<Book> Book {get; set; }
+    }
+}
---
+++ Views/BookList/Index.cshtml.cs
@@ -0,0 +1,21 @@
+using BookListRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Views.BookList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+            public IndexModel(ApplicationDbContext db)
+        {
+           _db = db;
+        }
+        public IEnumerable<Book> Books { get; set; }
+        public async Task OnGet()
+        {
+            Books = await _db.Book.ToListAsync();
+        }
+    }
+}
